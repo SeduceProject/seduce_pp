92a5d36e3775f618b8a6083340d80ef8b805da88
remy <remy.pottier@imt-atlantique.fr>
2020-01-20 13:12:28 +0000

    If no password is provided, the default c9 password is 'admin'

--- blueprints/webapp.py
+++ blueprints/webapp.py
@@ -50,7 +50,9 @@ def process_take():
         d.name = flask.request.form.get("name")
         d.public_key = flask.request.form.get("public_key")
         d.init_script = flask.request.form.get("init_script")
-        d.c9pwd = flask.request.form.get("c9pwd")
+        pwd = flask.request.form.get("c9pwd")
+        if pwd.strip():
+            d.c9pwd = pwd
         d.environment = flask.request.form.get("environment")
         d.duration = flask.request.form.get("duration")
         d.start_date = datetime.datetime.utcnow()
