e1c79f6be78e586f32bc7d55d7b7c0b24430ff37
remy <remy.pottier@imt-atlantique.fr>
2020-02-13 09:31:10 +0000

    Change date format in log files

--- admin/update-state.sh
+++ admin/update-state.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 USER=pipi
 PWD=piseduce
-DB_NAME=piseduce_remy
+DB_NAME=piseduce
 NAME="node-21"
 NEW_STATE="created"
 
 echo "Name of the node:"
 echo -n "!! "
 read NAME
-id=$(grep -A 2 $NAME lib/config/cluster_config.py | grep id | sed 's/.*"\(.*\)",$/\1/')
+id=$(grep -A 2 $NAME ../lib/config/cluster_config.py | grep id | sed 's/.*"\(.*\)",$/\1/')
 STATE=$(mysql -u$USER -p$PWD $DB_NAME -Ne "SELECT state FROM deployment WHERE state != 'destroyed' AND server_id = '$id';")
 echo "New state (current=$STATE):"
 echo -n "!! "
